c46124c7276164f8b1bc06f556585d3e235a63ab
Stephan <stephan.f.white@gmail.com>
2026-07-07 22:44:24 -0500

    Remove deprecated BallComponents.h file

--- src/ecs/BallComponents.h
+++
@@ -1,7 +0,0 @@
-// This file has been deprecated.
-// All components have been consolidated into src/ecs/Components.h
-// Please include and use Components.h instead.
-
-#pragma once
-
-#warning "BallComponents.h is deprecated. Use Components.h instead."
